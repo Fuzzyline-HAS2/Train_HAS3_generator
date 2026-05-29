f5a7e6674612ac8d5b4ea5cee36244632b65b2a1
Hyohyoju <julie08051@naver.com>
2026-05-29 11:40:45 +0900

    Firmware Update v14

---
+++ Train_HAS3_generator.ino
@@ -0,0 +1,46 @@
+#include <HAS2_Wifi.h>
+
+#include <HAS2_Wifi.h>
+
+#include <SecureOTA.h>
+
+/**
+ * @file Train_HAS3_generator.ino
+ * @author 김병준 (you@domain.com)
+ * @brief
+ * @version 1.0
+ * @date 2022-11-29
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#define FIRMWARE_VER 14
+#include "Train_HAS3_generator.h"
+
+void setup() {
+    Serial.begin(115200);
+    NeopixelInit();
+    RfidInit();
+    MotorInit();
+    EncoderInit();
+    NextionInit();
+    TimerInit();
+// has2wifi.Setup("KT_GiGA_6C64","ed46zx1198");
+ has2wifi.Setup("badland_shoot", "Has3");
+//    has2wifi.Setup("city");
+    ota.setLogStream(Serial);
+    ota.setOnSuccess([]() {
+        Serial.println("[OTA] ✅ 업데이트 성공! 재부팅합니다...");
+    });
+    ota.setOnSkip([]() {
+        Serial.println("[OTA] 이미 최신 버전입니다.");
+    });
+    ptrCurrentMode = WaitFunc;
+    ptrRfidMode = WaitFunc;
+    DataChanged();
+}
+void loop() {
+    ptrCurrentMode();
+    TimerRun();
+}
