12493c800972ac9c5518c55fed1573fe56f2f80e
DESKTOP-PLMAB5D\이효주 <julie08051@naver.com>
2026-06-26 18:32:29 +0900

    fix: 네오픽셀 색상 밝기 255 -> 100으로 하향

--- generator.h
+++ generator.h
@@ -90,17 +90,17 @@ const int NeopixelNum = 4;
 enum {GAUGE = 0, STARTER, DEVICESTATE, CIRCUIT};
 enum {WHITE = 0, RED, YELLOW, GREEN, BLUE, PURPLE, BLACK, BLUE0, BLUE1, BLUE2, BLUE3};
 // Neopixel 색상정보
-int color[11][3] = {    {255, 255, 255}, //WHITE
-                        {255, 0,   0},   //RED
-                        {255, 255, 0},   //YELLOW
-                        {0,   255, 0},   //GREEN
-                        {0,   0,   255}, //BLUE
-                        {255, 0,   255}, //PURPLE
+int color[11][3] = {    {100, 100, 100}, //WHITE
+                        {100, 0,   0},   //RED
+                        {100, 100, 0},   //YELLOW
+                        {0,   100, 0},   //GREEN
+                        {0,   0,   100}, //BLUE
+                        {100, 0,   100}, //PURPLE
                         {0,   0,   0},   //BLACK
                         {0,   0,   64},  //ENCODERBLUE0
                         {0,   0,   128}, //ENCODERBLUE1
                         {0,   0,   192}, //ENCODERBLUE2
-                        {0,   0,   255}}; //ENCODERBLUE3
+                        {0,   0,   100}}; //ENCODERBLUE3
 
 const int neopixel_num = 4; // 설치된 네오픽셀의 개수
 
